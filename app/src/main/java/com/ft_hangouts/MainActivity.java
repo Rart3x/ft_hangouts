2db006f8370200391e6c7a49c40e2443fe32bc8e
Rart3x <ramjattankeny971@gmail.com>
2024-01-23 08:10:30 +0100

    Add Footer, buttons on Footer, Redirection buttons->layouts

--- app/src/main/java/com/ft_hangouts/MainActivity.java
+++ app/src/main/java/com/ft_hangouts/MainActivity.java
@@ -1,14 +1,29 @@
 package com.ft_hangouts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.index);
+        setContentView(R.layout.footer);
     }
+
+    public void callsRedirection(View view) {
+        Intent intent = new Intent(this, Calls.class);
+        startActivity(intent);
+    }
+    public void contactsRedirection(View view) {
+        Intent intent = new Intent(this, Contacts.class);
+        startActivity(intent);
+    }
+    public void messagesRedirection(View view) {
+        Intent intent = new Intent(this, Messages.class);
+        startActivity(intent);
+    }
+
 }
