cc99768a450f3de0f91f1cdf4084a3dda22a9c40
Rart3x <ramjattankeny971@gmail.com>
2024-01-23 10:51:20 +0100

    DarkMode almost UP

--- app/src/main/java/com/ft_hangouts/MainActivity.java
+++ app/src/main/java/com/ft_hangouts/MainActivity.java
@@ -3,14 +3,20 @@ package com.ft_hangouts;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean colorMode = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.footer);
+        setContentView(R.layout.index);
+
+//        setContentView(R.layout.footer);
     }
 
     public void callsRedirection(View view) {
@@ -25,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Messages.class);
         startActivity(intent);
     }
+
+//    public void settingsRedirection(View view) {
+//        Intent intent = new Intent(this, Settings.class);
+//        startActivity(intent);
+//    }
+//
+    public void switchColorMode(View view) {
+        colorMode = !colorMode;
+    }
+
 
 }
